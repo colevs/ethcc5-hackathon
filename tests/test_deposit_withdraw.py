2b38320da9146f130092bec2888966455b1c0683
omurovec <owenmurovec@gmail.com>
2022-07-22 17:50:11 +0200

    fix: test_widthraw_deposit unsigned transactions

--- tests/test_deposit_withdraw.py
+++ tests/test_deposit_withdraw.py
@@ -5,12 +5,14 @@ import random
 MAX_SAMPLES = 50
 
 
-def test_1st_deposit_and_last_withdraw(initial_prices, crypto_swap, coins, token, accounts):
+def test_1st_deposit_and_last_withdraw(
+    initial_prices, crypto_swap, coins, token, accounts
+):
     user = accounts[1]
     quantities = [10**36 // p for p in [10**18] + initial_prices]  # $2 worth
     for coin, q in zip(coins, quantities):
-        coin._mint_for_testing(user, q)
-        coin.approve(crypto_swap, 2**256-1, sender=user)
+        coin._mint_for_testing(user, q, sender=user)
+        coin.approve(crypto_swap, 2**256 - 1, sender=user)
 
     # Very first deposit
     crypto_swap.add_liquidity(quantities, 0, sender=user)
@@ -25,7 +27,9 @@ def test_1st_deposit_and_last_withdraw(initial_prices, crypto_swap, coins, token
     assert token.balanceOf(user) == token.totalSupply() == 0
 
 
-def test_second_deposit(initial_prices, crypto_swap_with_deposit, token, coins, accounts):
+def test_second_deposit(
+    initial_prices, crypto_swap_with_deposit, token, coins, accounts
+):
     min_value = 10**16
     max_value = 10**9 * 10**18
 
@@ -36,7 +40,9 @@ def test_second_deposit(initial_prices, crypto_swap_with_deposit, token, coins, 
             values[i] = random.randint(10**16, 10**9 * 10**18)
 
         user = accounts[1]
-        amounts = [v * 10**18 // p for v, p in zip(values, [10**18] + initial_prices)]
+        amounts = [
+            v * 10**18 // p for v, p in zip(values, [10**18] + initial_prices)
+        ]
 
         xp = [10**6 * 10**18] * 2
         for i in range(2):
@@ -44,17 +50,23 @@ def test_second_deposit(initial_prices, crypto_swap_with_deposit, token, coins, 
         _A = crypto_swap_with_deposit.A()
         _gamma = crypto_swap_with_deposit.gamma()
         _D = sim.solve_D(_A, _gamma, xp)
-        safe = all(f >= 1.1e16 and f <= 0.9e20 for f in [_x * 10**18 // _D for _x in xp])
+        safe = all(
+            f >= 1.1e16 and f <= 0.9e20 for f in [_x * 10**18 // _D for _x in xp]
+        )
 
         for c, v in zip(coins, amounts):
-            c._mint_for_testing(user, v)
+            c._mint_for_testing(user, v, sender=user)
 
         try:
             calculated = crypto_swap_with_deposit.calc_token_amount(amounts)
             measured = token.balanceOf(user)
             d_balances = [crypto_swap_with_deposit.balances(i) for i in range(2)]
-            crypto_swap_with_deposit.add_liquidity(amounts, int(calculated * 0.999), sender=user)
-            d_balances = [crypto_swap_with_deposit.balances(i) - d_balances[i] for i in range(2)]
+            crypto_swap_with_deposit.add_liquidity(
+                amounts, int(calculated * 0.999), sender=user
+            )
+            d_balances = [
+                crypto_swap_with_deposit.balances(i) - d_balances[i] for i in range(2)
+            ]
             measured = token.balanceOf(user) - measured
 
             assert abs(calculated - measured) / measured < 1e-10
@@ -69,32 +81,37 @@ def test_second_deposit(initial_prices, crypto_swap_with_deposit, token, coins, 
         crypto_swap_with_deposit.get_dy(0, 1, 10**16)
 
 
-def test_second_deposit_one(crypto_swap_with_deposit, token, coins, accounts):
+def test_second_deposit_one(
+    initial_prices, crypto_swap_with_deposit, token, coins, accounts
+):
     min_value = 10**16
     max_value = 10**6 * 10**18
 
     max_i = 1
 
     for x in range(MAX_SAMPLES):
-        for i in range(max_i+1):
+        for i in range(max_i + 1):
             value = random.randint(min_value, max_value)
 
             user = accounts[1]
             amounts = [0] * 2
             amounts[i] = value * 10**18 // ([10**18] + initial_prices)[i]
             for c, v in zip(coins, amounts):
-                c._mint_for_testing(user, v)
+                c._mint_for_testing(user, v, user)
 
             calculated = crypto_swap_with_deposit.calc_token_amount(amounts)
             measured = token.balanceOf(user)
             d_balances = [crypto_swap_with_deposit.balances(i) for i in range(2)]
-            crypto_swap_with_deposit.add_liquidity(amounts, int(calculated * 0.999), sender=user)
-            d_balances = [crypto_swap_with_deposit.balances(i) - d_balances[i] for i in range(2)]
+            crypto_swap_with_deposit.add_liquidity(
+                amounts, int(calculated * 0.999), sender=user
+            )
+            d_balances = [
+                crypto_swap_with_deposit.balances(i) - d_balances[i] for i in range(2)
+            ]
             measured = token.balanceOf(user) - measured
 
             assert abs(calculated - measured) / measured < 1e-10
             assert tuple(amounts) == tuple(d_balances)
-
 
 
 def test_immediate_withdraw(crypto_swap_with_deposit, token, coins, accounts):
@@ -111,10 +128,11 @@ def test_immediate_withdraw(crypto_swap_with_deposit, token, coins, accounts):
             measured = [c.balanceOf(user) for c in coins]
             d_balances = [crypto_swap_with_deposit.balances(i) for i in range(2)]
             crypto_swap_with_deposit.remove_liquidity(
-                    token_amount,
-                    [int(0.999 * e) for e in expected],
-                    sender=user)
-            d_balances = [d_balances[i] - crypto_swap_with_deposit.balances(i) for i in range(2)]
+                token_amount, [int(0.999 * e) for e in expected], sender=user
+            )
+            d_balances = [
+                d_balances[i] - crypto_swap_with_deposit.balances(i) for i in range(2)
+            ]
             measured = [c.balanceOf(user) - m for c, m in zip(coins, measured)]
 
             for e, m in zip(expected, measured):
@@ -124,10 +142,14 @@ def test_immediate_withdraw(crypto_swap_with_deposit, token, coins, accounts):
 
     else:
         with ape.reverts():
-            crypto_swap_with_deposit.remove_liquidity(token_amount, [0] * 2, sender=user)
+            crypto_swap_with_deposit.remove_liquidity(
+                token_amount, [0] * 2, sender=user
+            )
 
 
-def test_immediate_withdraw_one(crypto_swap_with_deposit, token, coins, accounts):
+def test_immediate_withdraw_one(
+    initial_prices, crypto_swap_with_deposit, token, coins, accounts
+):
     user = accounts[1]
 
     min_value = 10**12
@@ -136,7 +158,7 @@ def test_immediate_withdraw_one(crypto_swap_with_deposit, token, coins, accounts
     max_i = 1
 
     for x in range(MAX_SAMPLES):
-        for i in range(max_i+1):
+        for i in range(max_i + 1):
             token_amount = random.randint(min_value, max_value)
 
             if token_amount >= token.totalSupply():
@@ -151,10 +173,15 @@ def test_immediate_withdraw_one(crypto_swap_with_deposit, token, coins, accounts
                 _gamma = crypto_swap_with_deposit.gamma()
                 _D = crypto_swap_with_deposit.D() * (_supply - token_amount) // _supply
                 xp[i] = sim.solve_x(_A, _gamma, xp, _D, i)
-                safe = all(f >= 1.1e16 and f <= 0.9e20 for f in [_x * 10**18 // _D for _x in xp])
+                safe = all(
+                    f >= 1.1e16 and f <= 0.9e20
+                    for f in [_x * 10**18 // _D for _x in xp]
+                )
 
                 try:
-                    calculated = crypto_swap_with_deposit.calc_withdraw_one_coin(token_amount, i)
+                    calculated = crypto_swap_with_deposit.calc_withdraw_one_coin(
+                        token_amount, i
+                    )
                 except Exception:
                     if safe:
                         raise
@@ -163,15 +190,24 @@ def test_immediate_withdraw_one(crypto_swap_with_deposit, token, coins, accounts
                 measured = coins[i].balanceOf(user)
                 d_balances = [crypto_swap_with_deposit.balances(k) for k in range(2)]
                 try:
-                    crypto_swap_with_deposit.remove_liquidity_one_coin(token_amount, i, int(0.999 * calculated), sender=user)
+                    crypto_swap_with_deposit.remove_liquidity_one_coin(
+                        token_amount, i, int(0.999 * calculated), sender=user
+                    )
                 except Exception:
                     # Check if it could fall into unsafe region here
-                    frac = (d_balances[i] - calculated) * ([10**18] + initial_prices)[i] // crypto_swap_with_deposit.D()
+                    frac = (
+                        (d_balances[i] - calculated)
+                        * ([10**18] + initial_prices)[i]
+                        // crypto_swap_with_deposit.D()
+                    )
                     if frac > 1.1e16 or frac < 0.9e20:
                         raise
                     else:
                         return
-                d_balances = [d_balances[k] - crypto_swap_with_deposit.balances(k) for k in range(2)]
+                d_balances = [
+                    d_balances[k] - crypto_swap_with_deposit.balances(k)
+                    for k in range(2)
+                ]
                 measured = coins[i].balanceOf(user) - measured
 
                 assert calculated == measured
