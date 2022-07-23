ace6f73d1025eba7bc1a9cbe58a31d19d2ceab6c
Cole <colervs@gmail.com>
2022-07-23 10:59:18 -0700

    deploy pool and lp token

--- scripts/setup.py
+++ scripts/setup.py
@@ -7,8 +7,10 @@ class Info:
     deployer: str
     user: str
     tokens: [str]
-    erc20_list: []
-    erc4626_list: []
+    erc20_list: [object]
+    erc4626_list: [object]
+    pool: object
+    lp_token: object
 
 
 def setup():
@@ -18,6 +20,8 @@ def setup():
     tokens = ["USDC", "ETH"]
     prepends = ["y", "a"]
     mint_quantity = 5 * 10**6 * 10**18  # 5 million
+
+    initial_prices = [int(0.8 * 10**18)]
 
     erc20_list = [None] * len(tokens)
     erc4626_list = [None] * len(tokens)
@@ -44,4 +48,31 @@ def setup():
             erc20_list[i].approve(erc4626_list[i], 2**256 - 1)
             erc4626_list[i].deposit(mint_quantity)
 
-    return Info(deployer, user, tokens, erc20_list, erc4626_list)
+
+    with boa.env.prank(deployer):
+        lp_token = boa.load(
+            "contracts/CurveTokenV5.vy",
+            "Curve EUR-USD",
+            "crvEURUSD"
+            )
+
+
+        pool = boa.load(
+            "contracts/CurveCryptoSwap4626.vy",
+            deployer,
+            deployer,
+            90 * 2**2 * 10000,  # A
+            int(2.8e-4 * 1e18),  # gamma
+            int(5e-4 * 1e10),  # mid_fee
+            int(4e-3 * 1e10),  # out_fee
+            10**10,  # allowed_extra_profit
+            int(0.012 * 1e18),  # fee_gamma
+            int(0.55e-5 * 1e18),  # adjustment_step
+            0,  # admin_fee
+            600,  # ma_half_time
+            initial_prices[0],
+            lp_token.address,
+            [erc4626.address for erc4626 in erc4626_list]
+        )
+
+    return Info(deployer, user, tokens, erc20_list, erc4626_list, pool, lp_token)
